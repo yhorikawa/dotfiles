0a8a992691efa44059c715ec42aa659f7d66038a
horikawa.yoshiki <75514868+yhorikawa@users.noreply.github.com>
2023-10-17 21:04:48 +0900

    test

--- vscode-devcontainer.sh
+++ vscode-devcontainer.sh
@@ -1,1 +1,52 @@
 #!/usr/bin/env bash
+
+DOTFILES_DIR=$(pwd)
+
+INSTALL_MAPPINGS="
+zsh/.zshrc                        .zshrc
+bash/.bashrc                      .bashrc
+.config/starship.toml             .config/starship.toml
+.config/sheldon/zsh/plugins.toml  .config/sheldon/zsh/plugins.toml
+.config/sheldon/bash/plugins.toml .config/sheldon/bash/plugins.toml
+.config/git/ignore                .config/git/ignore
+.vim                              .vim
+"
+
+install_starship() {
+    echo "Installing Starship..."
+    sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+}
+
+install_sheldon() {
+    echo "Installing Sheldon..."
+    curl --proto '=https' -fLsS https://rossmacarthur.github.io/install/crate.sh \
+    |  bash -s -- --repo rossmacarthur/sheldon --to ~/.local/bin
+}
+
+# Function to create symbolic links based on provided mappings
+create_symbolic_links() {
+    echo "Creating symbolic links..."
+    echo "$INSTALL_MAPPINGS" | while read MAP
+    do
+        [ ! -n "$MAP" ] && continue
+
+        ARGS=($MAP)
+        src="${ARGS[0]}"
+        dst="${ARGS[1]}"
+
+        mkdir -p "$(dirname "$HOME/$dst")"
+
+        if [ -e "$HOME/$dst" ] || [ -h "$HOME/$dst" ]; then
+            echo "Existing file or link detected at $HOME/$dst. Skipping..."
+            continue
+        fi
+
+        ln -s "$DOTFILES_DIR/$src" "$HOME/$dst"
+        echo "Created symbolic link: $src -> $dst"
+    done
+}
+
+install_starship
+install_sheldon
+create_symbolic_links
+echo "Done!"
