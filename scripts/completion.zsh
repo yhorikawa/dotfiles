9bc6f37d6e1f5669a8dfd59b4cc99de2732e6900
horikawa.yoshiki <75514868+yhorikawa@users.noreply.github.com>
2024-06-03 16:00:47 +0900

    add: wezterm-completion

--- scripts/completion.zsh
+++ scripts/completion.zsh
@@ -2,3 +2,7 @@
 if (( ${+commands[gh]} )); then
   gh completion -s zsh > "$XDG_DATA_HOME"/zsh/completions/_gh
 fi
+
+if (( ${+commands[wezterm]} )); then
+  wezterm shell-completion --shell zsh > "$XDG_DATA_HOME"/zsh/completions/_wezterm
+fi
