056e6f6537d2df620b880cf3ecfb5e177414f611
horikawa.yoshiki <75514868+yhorikawa@users.noreply.github.com>
2024-10-03 14:12:09 +0900

    add: sheldon completion

--- scripts/completion.zsh
+++ scripts/completion.zsh
@@ -6,3 +6,7 @@ fi
 if (( ${+commands[wezterm]} )); then
   wezterm shell-completion --shell zsh > "$XDG_DATA_HOME"/zsh/completions/_wezterm
 fi
+
+if (( ${+commands[sheldon]} )); then
+  sheldon completions --shell zsh > "$XDG_DATA_HOME"/zsh/completions/_sheldon
+fi
